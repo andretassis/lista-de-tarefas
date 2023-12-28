e0b33305ff5e10643d9613ed34fdf93cc38eff58
Andre Tassis <tassisandre@gmail.com>
2023-12-28 08:48:51 -0300

    chore(): acerto de rotas

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client'
 import './index.scss'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Tasks from './views/Tasks';
+import Edit from './components/Edit/Edit.jsx';
+import Delete from './components/Delete/Delete.jsx';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Tasks />,
+  },
+  {
+    path: "/edit",
+    element: <Edit />,
+  },
+  {
+    path: "/delete",
+    element: <Delete />,
   },
 ]);
 
